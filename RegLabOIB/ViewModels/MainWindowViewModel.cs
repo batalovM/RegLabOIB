0047844e91525e9f8dec7e9e42a5573de42e3265
batalovM <b.maksim1708@gmail.com>
2023-12-14 16:41:23 +0700

    Initial commit

---
+++ RegLabOIB/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace RegLabOIB.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ RegLabOIB/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace RegLabOIB.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
