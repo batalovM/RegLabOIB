f65f9100d2710198a63f7ac44af264fe8eb03d16
cute-dot <sasha.surikov.03@mail.ru>
2023-12-14 17:20:43 +0700

    sfdd

--- RegLabOIB/ViewModels/MainWindowViewModel.cs
+++ RegLabOIB/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace RegLabOIB.ViewModels;
+﻿using System.Windows.Input;
+using ReactiveUI;
+using RegLabOIB.Views;
+
+namespace RegLabOIB.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
- 
+    public ICommand OpenRegWindowCommand { get; }
+
+    public MainWindowViewModel()
+    {
+       
+    }
+
+    private void OpenSecondWindow()
+    {
+        var secondWindow = new RegistationWindow();
+        secondWindow.Show();
+    }
 }
